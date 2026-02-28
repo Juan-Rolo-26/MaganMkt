eca81446597d45e15835d5dbc1baf3275defae4c
Juan Pablo Rolo <juanpablorolo2007@gmail.com>
2026-02-28 17:23:56 +0000

    a

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo Backend")
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(3000);
